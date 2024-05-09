394d55b29030200fcd06846d8545b35c44f7aa8c
kashishek <skorpet0k@gmail.com>
2024-05-09 13:13:10 +0200

    commit

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request, session, redirect, url_for
-from flask_socketio import SocketIO
+#from flask import *
+from flask_socketio import SocketIO, join_room, leave_room, send
 import random
 from string import ascii_uppercase
 
@@ -38,7 +39,7 @@ def home():
         room = code
         if create!=False:
             room = generate_code(4)
-            rooms[room] = {"members":0, "messeges": []}
+            rooms[room] = {"members":0, "messages": []}
         elif code not in rooms:
             return render_template("home.html", error="zły kodzik do pokoju?", code=code, name=name)
 
@@ -50,7 +51,54 @@ def home():
 
 @app.route("/room")
 def room():
-    return render_template("room.html")
+    room = session.get("room")
+    if room is None or session.get("name") is None or room not in rooms:
+        return redirect(url_for("home"))
+    
+    return render_template("room.html", code=room, messages=rooms[room]["messages"])
+
+@socketio.on("message")
+def message(data):
+    room = session.get("room")
+    if room not in rooms:
+        return
+    
+    content = {
+        "name": session.get("name"),
+        "message": data["data"]
+    }
+    send(content, to=room)
+    rooms[room]["messages"].append(content)
+    print(f"{session.get('name')} said: {data['data']}")
+
+
+@socketio.on("connect")
+def connect(auth):
+    room = session.get("room")
+    name = session.get("name")
+    if not room or not name:
+        return
+    if room not in rooms:
+        leave_room(room)
+        return
+    
+    join_room(room)
+    send({"name": name, "message": "has entered the room"}, to=room)
+    rooms[room]["members"] += 1
+    print(f"{name} joined room {room}")
+
+@socketio.on("disconnect")
+def disconnect():
+    room = session.get("room")
+    name = session.get("name")
+    leave_room(room)
+    
+    if room in rooms:
+        rooms[room]["members"] -=1
+        if rooms[room]["members"]<=0:
+            del rooms[room]
+    send({"name": name, "message": "has left the room"}, to=room)
+    print(f"{name} left room {room}")
 
 if __name__=="__main__":
     socketio.run(app, debug=True)
